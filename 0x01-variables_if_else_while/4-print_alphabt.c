470f2c44cac6e208fb6ed46067d771d679c22a04
abderrahmannader <abderrahman.nader10@gmail.com>
2023-08-11 21:18:18 -0700

    c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char wor = 'a';
+
+	while (wor <= z)
+	{
+		if (wor == q || wor == e)
+			wor++;
+		putchar(wor);
+		ch++;
+	}
+	putchar('\n');
+
+	return (o);
+}
