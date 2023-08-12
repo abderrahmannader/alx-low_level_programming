c988db49f182512fd845ec45914aa1536b32aa73
abderrahmannader <abderrahman.nader10@gmail.com>
2023-08-11 19:50:53 -0700

    c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: 0
+ */
+
+int main(void)
+{	
+	char al = 'a';
+
+	while (al <= 'z')
+	{
+		putchar(al);
+		al++;
+	}
+	putchar('\n');
+
+	return (0);
+}
