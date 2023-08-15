0de9b04f4e203c67a50c470189ee433f59727d9c
abderrahmannader <abderrahman.nader10@gmail.com>
2023-08-15 15:37:45 -0700

    c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ *Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	int alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		_putchar(alp);
+	}
+	_putchar('\n');
+}
