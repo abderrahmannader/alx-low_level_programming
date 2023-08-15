409fb26abf43c2912577e869502831ec01da1664
abderrahmannader <abderrahman.nader10@gmail.com>
2023-08-15 16:21:18 -0700

    tie

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,13 +1,14 @@
-#include <unistd.h>
 #include "main.h"
-
+#include <unistd.h>
 /**
- *_putchar - writes the character c
- *@c: The character
- *Return: 0
+ * _putchar - character c
  *
+ * @c:character
+ *
+ * Return: 0
  */
-int _putchar(char c);
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+#define main_h
 
-#include <stdio.h>
-
-int _putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
 
 #endif
-
-
