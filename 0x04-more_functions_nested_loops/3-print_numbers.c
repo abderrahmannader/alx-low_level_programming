4b87c863b1ee61e103fcc0342ed1deb72ab5e6e3
abderrahmannader <abderrahman.nader10@gmail.com>
2023-08-17 15:55:09 -0700

    done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 to 9
+ * return: void
+ */
+
+void print_numbers(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
+
