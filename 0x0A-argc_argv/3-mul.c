c127d23e81b8955a541409e22be54248fad4697f
abderrahmannader <abderrahman.nader10@gmail.com>
2023-09-01 11:19:54 -0700

    done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments
+ * @argc: number
+ * @argv: array
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
